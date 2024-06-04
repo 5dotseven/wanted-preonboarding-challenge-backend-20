387940729c338818beed079997e71def2a99ec6e
[Cloud]나정균 <dotseven57@gmail.com>
2024-06-04 23:44:56 +0900

    feat: 초기세팅

---
+++ src/main/java/com/example/wanted_6/Wanted6Application.java
@@ -0,0 +1,13 @@
+package com.example.wanted_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wanted6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wanted6Application.class, args);
+	}
+
+}
